50b1a42af227b80d71e25036f1182a19c56ed722
Akhil <pletiakhil100@gmail.com>
2023-01-12 18:46:26 +0530

    Initial Commit

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let main = document.getElementById('main-display');
 let previous = 0;
 let num = 0
-function showInDisplay(value){
-    if (value === NaN) {
-        
-    }
+function showInDisplay(value) {
+    // if (value === NaN) {
+
+    // }
     main.textContent += `${value}`;
     num = parseInt(main.textContent);
 }
